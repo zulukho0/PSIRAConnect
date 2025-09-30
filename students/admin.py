fdf3e611123fa066c737789b84577ff4ded2cd73
zulukho0 <zulukho0@gmail.com>
2025-09-30 10:57:03 +0200

    add: created_at and updated_at field to student db then ran migrations

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'second_name' 'last_name', 'id_number', 'contact', 'created_at')
+    search_fields = ('first_name', 'last_name', 'id_number')
--- students/models.py
+++ students/models.py
@@ -7,6 +7,8 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     id_number = models.CharField(max_length=13, unique=True)
     contact_number = models.CharField(max_length=10, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.id_number})"
