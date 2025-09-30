bf72a0f3394c594ba614caa4a3c820e33e99a420
zulukho0 <zulukho0@gmail.com>
2025-09-30 13:27:46 +0200

    Add: edit subject undernew course functionality

--- courses/admin.py
+++ courses/admin.py
@@ -2,20 +2,19 @@ from django.contrib import admin
 from .models import Course, SubjectTemplate
 
 # Register your models here.
+
+# Inline for SubjectTemplate under Course
+class SubjectTemplateInline(admin.TabularInline):
+    model = SubjectTemplate
+    extra = 0
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    # Fields to display in the list view
-    list_display = (
-        'grade', 
-        'description',
-        'price', 
-        'created_at', 
-        'updated_at'
-    )
-
-    # Fields you can search by
+    list_display = ('grade', 'description', 'price', 'created_at')
+    inlines = [SubjectTemplateInline]
     search_fields = ('grade',)
 
+# Register SubjectTemplate separately to allow direct access
 @admin.register(SubjectTemplate)
 class SubjectTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'course', 'max_theory', 'max_practical')
