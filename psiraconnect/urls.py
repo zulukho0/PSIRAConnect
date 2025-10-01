8437ee9e6581becea41253f4cad1f42250cc0011
zulukho0 <zulukho0@gmail.com>
2025-10-01 22:24:49 +0200

    register api urls to main app

--- psiraconnect/urls.py
+++ psiraconnect/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('results/', include('results.urls')),
+    path('api/', include('api.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
