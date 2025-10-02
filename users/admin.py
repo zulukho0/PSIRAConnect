c0dc01d36790a494fb1c2b9a27d2188b17e708e5
zulukho0 <zulukho0@gmail.com>
2025-10-02 13:08:56 +0200

    feat(students): register CustomUser with enhanced fields and filters in admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Fields shown in the admin list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+
+    # Fields to filter by in the right sidebar
+    list_filter = ('role', 'is_staff', 'is_active')
+
+    # Fieldsets used for editing users
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('role', 'is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields used when creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
