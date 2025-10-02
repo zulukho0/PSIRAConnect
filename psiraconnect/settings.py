9b9e65834c0280c3449344d1ed23f9b5bee5b2c1
zulukho0 <zulukho0@gmail.com>
2025-10-02 12:40:07 +0200

    "Add initial migrations for CustomUser model with roles and permissions system."

--- psiraconnect/settings.py
+++ psiraconnect/settings.py
@@ -148,3 +148,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
+
+#Declare custom usermodel
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('ADMIN', 'Admin'),
+        ('MANAGER', 'Manager'),
+        ('LECTURER', 'Lecturer'),
+        ('CLERK', 'Clerk'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='CLERK')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
