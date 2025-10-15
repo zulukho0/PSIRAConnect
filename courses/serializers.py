5cd4639b865a627597dba76e4b6a9b3d6f6d29ee
zulukho0 <zulukho0@gmail.com>
2025-10-15 13:24:32 +0200

    feat(courses): add course creation serializer with subject handling

--- courses/serializers.py
+++ courses/serializers.py
@@ -8,6 +8,23 @@ class SubjectTemplateSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     subjects = SubjectTemplateSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Course
         fields = '__all__'
+
+class CourseCreateSerializer(serializers.ModelSerializer):
+    subjects = SubjectTemplateSerializer(many=True, required=False)
+    
+    class Meta:
+        model = Course
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        subjects_data = validated_data.pop('subjects', [])
+        course = Course.objects.create(**validated_data)
+        
+        for subject_data in subjects_data:
+            SubjectTemplate.objects.create(course=course, **subject_data)
+        
+        return course
