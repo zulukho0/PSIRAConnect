2b49fcdd4b969e88ae0610dd10f9a1cafdb382ee
zulukho0 <zulukho0@gmail.com>
2025-10-15 13:24:17 +0200

    feat(courses): add subject template viewset and course create serializer

--- courses/api_views.py
+++ courses/api_views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
-from courses.models import Course
-from .serializers import CourseSerializer
+from rest_framework.response import Response
+from courses.models import Course, SubjectTemplate
+from .serializers import CourseSerializer, SubjectTemplateSerializer, CourseCreateSerializer
 from users.permissions import RolePermission
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [RolePermission]
+    
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CourseCreateSerializer
+        return CourseSerializer
+
+class SubjectTemplateViewSet(viewsets.ModelViewSet):
+    queryset = SubjectTemplate.objects.all()
+    serializer_class = SubjectTemplateSerializer
     permission_classes = [RolePermission]
